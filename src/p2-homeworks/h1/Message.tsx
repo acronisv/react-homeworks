f06849e93d70e5676d76e4d2d904438053c10de3
acronisv <acronisv@gmail.com>
2022-07-20 01:39:50 +0300

    Initial commit

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type propsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string
+}
+function Message(props: propsType) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Message
