56ed99738f5b7a8d52eb0eab976aee0747f90dfe
acronisv <acronisv@gmail.com>
2022-08-24 00:57:47 +0300

    Add HW6

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Junior() {
+    return (
+        <div>
+            HW7-HW11
+        </div>
+    )
+}
+
+export default Junior
---
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function JuniorPlus() {
+    return (
+        <div>
+            HW12 - till the end
+        </div>
+    )
+}
+
+export default JuniorPlus
